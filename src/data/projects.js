b813bef9f3328ab7d03f09f3781d181457f8b279
somsokraksa534-afk <somsokraksa534@gmail.com>
2026-08-07 14:36:35 +0700

    update

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,26 +1,30 @@
+import Web from "../assets/logocard/images.jpg";
+import Graphic from "../assets/logocard/451_banner.webp";
+import Studio from "../assets/logocard/images1.jpg";
+import UXUI from "../assets/logocard/image2.png";
 export const projects = [
   {
     category: "Web Development",
     title: "Web Development",
-    image: "../src/assets/logocard/images.jpg",
+    image: Web,
     color: "from-indigo-500 to-blue-600",
   },
   {
     category: "Graphic Design",
     title: "Graphic Design",
-    image: "../src/assets/logocard/451_banner.webp",
+    image: Graphic,
     color: "from-pink-500 to-rose-600",
   },
   {
     category: "Studio Photography",
     title: "Studio Photography",
-    image: "../src/assets/logocard/images1.jpg",
+    image: Studio,
     color: "from-amber-500 to-orange-600",
   },
   {
     category: "UX / UI Design",
     title: "UX / UI Design",
-    image: "../src/assets/logocard/image2.png",
+    image: UXUI,
     color: "from-purple-500 to-violet-600",
   },
 ];
